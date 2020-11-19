c828eda8b24a411e1ab3e515efe2124e9c1c32a5
RicardoQuirino <ricardoquirino72@gmail.com>
2020-11-19 16:03:39 -0300

    first commit

---
+++ src/components/Products/data.js
@@ -0,0 +1,59 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+import dessert1 from '../../images/dessert-1.jpg'
+import dessert2 from '../../images//dessert-2.jpg'
+import dessert3 from '../../images/dessert-3.jpg'
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.99',
+    button: 'Add to Cart'
+  }
+]
+
+export const productDataTwo = [
+  {
+    img: dessert1,
+    alt: 'Cone Ice Cream',
+    name: 'Filatova',
+    desc: 'A cone of ice cream with cookies and fruit around, flat lay in white background',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: dessert2,
+    alt: 'Chocolate Square',
+    name: 'Massimo',
+    desc: 'Almond, hazelnut and pistachio cremino',
+    price: '$18.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: dessert3,
+    alt: 'Lava Sundae',
+    name: 'Emile Lava',
+    desc: 'A glass of ice cream, coconut, chocolate, pistachios and oreo',
+    price: '$16.99',
+    button: 'Add to Cart',
+  }]
